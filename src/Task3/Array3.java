1570aec2f816aacd36dd8230253dfa0bb387f4c2
Mihail Gustiuc - Moldova <MihailG@gilat.com>
2023-02-15 18:43:45 +0200

    Edited

--- src/Task3/Array3.java
+++ src/Task3/Array3.java
@@ -14,6 +14,21 @@ public class Array3 {
         int[] copiedArray = Arrays.copyOf(firstArray,firstArrayLength);
         System.out.println("Copied Array: " + Arrays.toString(copiedArray));
 
+        copyArray();
+
     }
 
+    private static void copyArray(){
+        int[]firstArray ={2,4,6,7,8,5,1};
+        int[]secondArray = new int[firstArray.length];
+        for(int i=0 ;i< firstArray.length;i++){
+            secondArray[i]=firstArray[i];
+        }
+
+        System.out.println("Original Array: " + Arrays.toString(firstArray));
+        System.out.println("Copied Array: " + Arrays.toString(secondArray));
+    }
+
+
 }
+
